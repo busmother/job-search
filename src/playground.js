f48f55094ceedd5d31ddb0352cc739fba8342695
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-16 18:02:44 -0400

    add test code

---
+++ src/playground.js
@@ -0,0 +1,16 @@
+export const evenOrOdd = (number) => {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+};
+
+export const multiply = (num1, num2) => {
+  let total = 0;
+  for (let i = 0; i < num1; i++) {
+    total += num2;
+  }
+
+  return total;
+};
