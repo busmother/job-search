0c53bd18d7d1bd326130df1c383edd9f3795f19f
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-16 20:01:32 -0400

    update import

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <MainNav />
+  <main-nav />
 </template>
 
 <script>
-import MainNav from "./components/MainNav.vue";
+import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
   components: {
-    MainNav: MainNav,
+    MainNav,
   },
 };
 </script>
