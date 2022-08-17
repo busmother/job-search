3c0519d84771726e2c4a55196a4cd35b5a715e5a
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-17 13:38:22 -0400

    add nav items with data-test

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,12 +2,32 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div
-        class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-grey-1"
+        class="
+          flex flex-nowrap
+          h-full
+          px-8
+          mx-auto
+          border-b border-solid border-brand-gray-1
+        "
       >
-        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
-        <h2 class="flex items-center h-full ml-8">
-          Developed by {{ author.firstName }} {{ author.lastName }}
-        </h2>
+        <a :href="url" class="flex items-center h-full text-xl">{{
+          company
+        }}</a>
+
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="h-full ml-9 first:ml-0"
+              data-test="main-nav-list-item"
+            >
+              <a href="/" class="flex items-center h-full py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -19,10 +39,15 @@ export default {
   data() {
     return {
       company: "Bobo Careers",
-      author: {
-        firstName: "Ian",
-        lastName: "Marshall",
-      },
+      url: "https://careers.google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Bobo",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
