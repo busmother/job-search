57eba8df00882ce7ae5c8e08934bb95fd04c8220
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-17 13:38:46 -0400

    add displays menu items for navigation

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,22 +1,17 @@
 import { mount } from "@vue/test-utils";
+
 import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
-  it("describes company name", async () => {
+  it("displays company name", () => {
     const wrapper = mount(MainNav);
-    await wrapper.setData({
-      //wait until this is complete before moving onto the expect block
-      company: "Super Corp",
-    });
-    expect(wrapper.text()).toMatch("Super Corp");
+    expect(wrapper.text()).toMatch("Bobo Careers");
   });
 
   it("displays menu items for navigation", () => {
     const wrapper = mount(MainNav);
-    const navigationMenuItems = wrapper.findAll(
-      "[data-test='main-nav-list-item']" //update from "li"
-    ); 
-    const navigationMenuTexts = navigationMenuItems.map((item) => item.text);
+    const navigationMenuItems = wrapper.findAll("[data-test='main-nav-list-item']");
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
     expect(navigationMenuTexts).toEqual([
       "Teams",
       "Locations",
