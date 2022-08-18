3bd8f8a817dbbbefe579553210fd78795c0bde26
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-18 14:31:16 -0400

    basic profile image component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img v-bind:src="imageLink" class="w-8 h-8 object-contain rounded-3xl" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://townsquare.media/site/442/files/2014/12/Lee-Pace-630x420.jpg?w=980&q=75",
+    };
+  },
+};
+</script>
