c78f3662f9037f78e998c4136b9c870e8103ab52
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-22 19:51:33 -0400

    basic headline component

---
+++ src/components/Headline.vue
@@ -0,0 +1,63 @@
+<template>
+    <section>
+        <h1 class="font-bold tracking-tighter text-8xl mb-14">
+            <span :class="actionClasses">{{ action }}</span><br>
+             for everyone
+        </h1>
+        <h2>Find your next job at Bobo corp</h2>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Headline",
+    data() {
+        return {
+            action: "Build",
+            interval: null,
+        };
+    },
+    computed: {
+        actionClasses(){
+            return {
+                build: this.action === "Build",
+                create: this.action === "Create",
+                design: this.action === "Design",
+                code: this.action === "Code",
+            };
+        },
+    },
+    created() {
+        this.changeTitle()
+    },
+    beforeUnmount(){
+        clearInterval(this.interval);
+    },
+    methods: {
+            changeTitle(){
+                this.interval = setInterval(() => {
+                const actions = ["Build", "Create", "Design", "Code"]
+                const currentActionIndex = actions.indexOf(this.action)
+                const nextActionIndex = (currentActionIndex +1) % 4;
+                const nextAction = actions[nextActionIndex]
+                this.action = nextAction
+            }, 3000);
+        }
+    },
+};
+</script>
+
+<style scoped >
+.build {
+    color: #1a73e8;
+}
+.create {
+    color: #34a853;
+}
+.design {
+    color: #f9ab00
+}
+.code {
+    color: #d93025
+}
+</style>
