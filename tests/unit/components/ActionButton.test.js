c6caaba784d674cceabd050446fdc1c80c27a611
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-22 09:15:11 -0400

    basic button test

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,14 @@
+import { mount } from "@vue/test-utils";
+
+import ActionButton from "@/components/ActionButton";
+
+describe("ActionButton", () => {
+  it("renders text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "I'm so clickable",
+        type: "primary",
+      },
+    });
+  });
+});
