db6e5593dc4d52157f9754bc9c2ee11a61618909
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-22 21:44:17 -0400

    import JobSearchForm & remove redundant styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,18 +1,16 @@
 <template>
     <main>
-        <section class="flex flex-col h-screen pt-10 pb-20 bg-red-400">
+        <section class="flex flex-col h-screen pt-10 pb-20">
             <div class="grid grid-cols-12">
-                <div class="col-start-1 col-span-1 border border-blue-700"> 
-                    1 column
+                <div class="col-start-1 col-span-1"> 
                 </div>
-                <div class="col-start-2 col-span-5 border border-blue-700"> 
+                <div class="col-start-2 col-span-5"> 
                     <headline />
+                    <job-search-form />
                 </div>
-                <div class="col-start-7 col-span-5 border border-blue-700"> 
-                    5 columns
+                <div class="col-start-7 col-span-5"> 
                 </div>
-                <div class="col-start-12 col-span-5 border border-blue-700"> 
-                    1 column
+                <div class="col-start-12 col-span-5"> 
                 </div>
             </div>
         </section>
@@ -21,10 +19,12 @@
 
 <script>
 import Headline from '@/components/Headline.vue'
+import JobSearchForm from '@/components/JobSearchForm.vue'
 export default {
     name: "Hero",
     components: {
         Headline,
+        JobSearchForm
     },
 };
 </script>
