a4735ced8690a93d3cfd03eaaa6dde2e0a0041f2
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-22 21:50:18 -0400

    add basic styling and icon

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,5 +1,7 @@
 <template>
-    <form> I will be a form</form>
+    <form class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl">
+        <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3"/>
+    </form>
 </template>
 
 <script>
