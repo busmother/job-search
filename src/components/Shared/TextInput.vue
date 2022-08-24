5a4053b370d9a6439b0a675f65cdd44b374d6f83
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-23 21:40:35 -0400

    basic TextInput

---
+++ src/components/Shared/TextInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <input
+    type="text"
+    :value="value"
+    :placeholder="placeholder"
+    class="w-full text-lg font-normal focus:outline-none"
+    @input="handleInput"
+    />
+</template>
+
+<script>
+export default {
+    name: "TextInput",
+    props: {
+        placeholder: {
+            type: String,
+            required: false, // if you set required to false
+            default: "", // you need a default, but we can do ""
+        }
+    },
+    data() {
+        return {
+            value: "",
+        }
+    },
+    methods: {
+        handleInput($event) {
+            this.value = $event.target.value
+        }
+    }
+}
+</script>
