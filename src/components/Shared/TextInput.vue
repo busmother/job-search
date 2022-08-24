a6be6634923e072f238b40dc111fae38a2846dbe
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-24 10:46:43 -0400

    update with v-model

--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -1,7 +1,7 @@
 <template>
     <input
     type="text"
-    :value="value"
+    :value="modelValue"
     :placeholder="placeholder"
     class="w-full text-lg font-normal focus:outline-none"
     @input="handleInput"
@@ -14,23 +14,19 @@ export default {
     props: {
         placeholder: {
             type: String,
-            required: false, // if you set required to false
-            default: "", // you need a default, but we can do ""
+            required: false,
+            default: "",
+        },
+        modelValue: {
+            type: String,
+            required: true,
         }
     },
-    data() {
-        return {
-            value: "",
-        }
-    },
+    emits: ["update:modelValue"],
     methods: {
         handleInput($event) {
-            this.value = $event.target.value
-            this.$emit("handleInput", this.value); //custom event!
-            //$emit() takes two args, first is a unique name
-            //second is the information being passed
+            this.$emit("update:modelValue", $event.target.value); 
         }
-
     }
 }
 </script>
