08c27d8af46209e478c1199bac641e0640f2cb80
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-25 18:53:50 -0400

    add router-links

--- src/components/navigation/MainNav.vue
+++ src/components/navigation/MainNav.vue
@@ -10,21 +10,21 @@
           border-b border-solid border-brand-gray-1
         "
       >
-        <a :href="url" class="flex items-center h-full text-xl">{{
-          company
-        }}</a>
+        <router-link :to="{ name: 'Home' }" class="flex items-center h-full text-xl">
+          Bobo Careers
+        </router-link>
 
         <nav class="h-full ml-12">
           <ul class="flex h-full p-0 m-0 list-none">
             <li
               v-for="menuItem in menuItems"
-              :key="menuItem"
+              :key="menuItem.text"
               class="h-full ml-9 first:ml-0"
               data-test="main-nav-list-item"
             >
-              <a href="/" class="flex items-center h-full py-2.5">{{
-                menuItem
-              }}</a>
+            <router-link :to="menuItem.url" class="flex items-center h-full py-2.5">{{
+              menuItem.text
+              }}</router-link>
             </li>
           </ul>
         </nav>
@@ -59,15 +59,13 @@ export default {
   },
   data() {
     return {
-      company: "Bobo Careers",
-      url: "https://careers.google.com",
       menuItems: [
-        "Teams",
-        "Locations",
-        "Life at Bobo",
-        "How we hire",
-        "Students",
-        "Jobs",
+        { text: "Teams", url: "/", },
+        { text: "Locations", url: "/", },
+        { text: "Life at Bobo", url: "/", },
+        { text: "How we hir", url: "/", },
+        { text: "Students", url: "/", },
+        { text: "Jobs", url: "/jobs/results", },
       ],
       isLoggedIn: false,
     };
