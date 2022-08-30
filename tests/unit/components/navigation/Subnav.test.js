8552725d46ea8479b565cd8eb7e0aa071510a937
Ian Marshall <75340481+busmother@users.noreply.github.com>
2022-08-29 20:53:37 -0400

    correct typo on SubNav

--- tests/unit/components/navigation/Subnav.test.js
+++ tests/unit/components/navigation/Subnav.test.js
@@ -18,7 +18,7 @@ describe("Subnav", () => {
   describe("when user is on job page", () => {
     it("displays job count", () => {
       const routeName = "JobResults";
-      const wrapper = mount(Subnav, createConfig(routeName));
+      const wrapper = mount(SubNav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(true);
     });
@@ -27,7 +27,7 @@ describe("Subnav", () => {
   describe("when user is not on jobs page", () => {
     it("does NOT display job count", () => {
       const routeName = "Home";
-      const wrapper = mount(Subnav, createConfig(routeName));
+      const wrapper = mount(SubNav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(false);
     });
